1f523e969a46037e1c3165ac1e4b4514b383c292
Systmctl <artenvolemort@gmail.com>
2025-06-09 13:37:37 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,18 @@ const About = document.getElementById('About')
 const Writeups = document.getElementById('Writeups')
 const Journal = document.getElementById('Journal')
 
-About.addEventListener('click', () => location =  './about.html')
-Writeups.addEventListener('click', () => location =  './writeups.html')
-Journal.addEventListener('click', () => location =  './journal.html')
-Home.addEventListener('click', () => location = "./")
+About.addEventListener('click', () => window.location.href =  './about.html')
+Writeups.addEventListener('click', () => window.location.href =  './writeups.html')
+Journal.addEventListener('click', () => window.location.href =  './journal.html')
+Home.addEventListener('click', () => window.location.href = "./")
+
+const path = window.location.pathname;
+
+let buffer = '';
+document.addEventListener('keydown', (e) => {
+  buffer += e.key;
+  if (buffer.includes('systmctl')) {
+    alert('> Access granted. Welcome back, root.');
+    buffer = '';
+  }
+});
