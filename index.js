10edd7a58467b9231d7cd8ecd428e096ad7f0f16
Systmctl <artenvolemort@gmail.com>
2025-06-09 14:02:39 -0300

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ Home.addEventListener('click', () => window.location.href = "./")
 const path = window.location.pathname;
 
 let buffer = '';
-const easterEggSound = new Audio('./sounds/AcessGrantedSound.mp3');
+const easterEggSound = new Audio('./sounds/AccessGrantedSound.mp3');
 
 document.addEventListener('keydown', (e) => {
   buffer += e.key.toLowerCase();
